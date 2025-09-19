a8509c32bc594f4559af20382eb9bea274877bf8
saurabhbarhate112 <saurabh.barhate20@gmail.com>
2025-09-19 15:23:03 +0530

    Update and rename api.js to index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// This file exists to prevent Vercel build errors
+// The actual API is in /api/convert.js
+
+console.log('HTML to Image API is running on Vercel');
+
+module.exports = {
+  message: 'HTML to Image API - Use POST /api/convert'
+};
